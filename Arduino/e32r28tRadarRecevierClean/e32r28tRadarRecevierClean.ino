0569e9f3d17a6bc7863860cdf897698cb4bb5e82
Ryan Elia <ryanae4321@gmail.com>
2026-08-17 22:10:16 -0500

    Added Simulated Animation

--- Arduino/e32r28tRadarRecevierClean/e32r28tRadarRecevierClean.ino
+++ Arduino/e32r28tRadarRecevierClean/e32r28tRadarRecevierClean.ino
@@ -11,11 +11,13 @@
 #include <WiFi.h>
 
 #include "User_Setup.h"
-#include "screen.h" // this screen includes the text at the bottom of the display
+// #include "screen.h" // this screen includes the text at the bottom of the display
 #include "radar_clean.h" // this is the clean version of the screen
 
 // #include <Fonts/GFXFF/FreeSans12pt7b.h>
 
+#define SENSOR_TIMEOUT 2000
+#define NUM_SIM_TARGETS 5
 #define MAX_DISTANCE 427
 #define TFT_BL 21   // Backlight control pin (E32R28T)
 #define SD_CS 5
@@ -24,29 +26,35 @@ int x = 300;
 int y = 10;
 int r1 = 7;
 int r2 = 5;
+int centerX = 160;
+int centerY = 175;
 
 float duration, myData;
+float realDistance = -1; // Set default for startup
+float simDistance[NUM_SIM_TARGETS];
+float simAngle[NUM_SIM_TARGETS];
+float simSpeed[NUM_SIM_TARGETS];
 
-float distance = -1; // Set default for startup
+unsigned long lastPacketTime = 0;
 
 TFT_eSPI tft = TFT_eSPI();
 
-void (displayDistance(float distance)) {
-  char distDecimals[8];
-  int xStart = 125;
-  int yStart = 187;
-  int x = 160;
-  int centerY = 175;
-  int y = centerY - (distance / MAX_DISTANCE) * centerY;
-
+void displayDistance(float distance, float angle) {
+  float radius = (distance / MAX_DISTANCE) * centerY;
+  int x = centerX - radius * sin(angle);
+  int y = centerY - radius * cos(angle);
   if (y < 0) y = 0;
-  if (y > 175) y = 175;
 
   tft.fillCircle(x, y, 5, TFT_WHITE);
+}
+
+void displayDistanceText(float distance) {
+  int xStart = 125;
+  int yStart = 187;
+
   tft.setTextColor(TFT_RED);
   tft.setTextDatum(TL_DATUM);
-  // tft.setTextFont(1);
-  // tft.setTextSize(2);
+
   tft.setFreeFont(&FreeSans12pt7b);
   tft.setTextSize(1);
   int length = tft.drawNumber((int)distance, xStart, yStart);
@@ -56,14 +64,43 @@ void (displayDistance(float distance)) {
   length = tft.drawString(" - ", xStart, yStart+8);
   length = length + (tft.drawNumber((int)(distance* 10) % 10, xStart+length, yStart+6, 1) / 2);
   tft.setTextDatum(MC_DATUM);
-  tft.drawString("cm", xStart+length, yStart+18);
+  tft.drawString("cm", xStart+length, yStart+18); 
+}
+
+void displayDataText(int contacts, float averageDistance) {
+  char data[32];
+  int x = 220;
+  int y = 195;
+  tft.setTextColor(TFT_WHITE);
+  tft.setTextDatum(TL_DATUM);
+  tft.setTextFont(1);
+  tft.setTextSize(1);
+  sprintf(data, "C.X. %d/%.2f", contacts, averageDistance);
+  tft.drawString(data, x, y);
+  tft.setTextDatum(TR_DATUM);
+  x = 100;
+  tft.drawString("F.E.M.S. 9.47.2", x, y);
+}
+
+void simulateTarget(int i) {
+  simDistance[i] -= simSpeed[i];
+
+  if (simDistance[i] < 20) {
+    simDistance[i] = random(500, 4000) / 10.0;
+    simAngle[i] = random(-90, 90) * DEG_TO_RAD;
+    simSpeed[i] = random(1, 5);
+  }
+}
+
+bool sensorAvailable() {
+  return millis() - lastPacketTime < SENSOR_TIMEOUT;
 }
 
 void OnDataRecv(const esp_now_recv_info* info, const unsigned char* incomingData, int len) {
-   memcpy(&myData, incomingData, sizeof(myData));
+   memcpy(&realDistance, incomingData, sizeof(realDistance));
   //  Serial.print("Data Received: ");
-  //  Serial.println(myData);
-   distance = myData;
+  //  Serial.println(realDistance);
+   lastPacketTime = millis();
 }
 
 void headerImage() {
@@ -77,25 +114,42 @@ void radarSweep() {
   while (r < 238) {
     headerImage();
     tft.drawArc(160, 175, r + 5, r, 90, 270, TFT_WHITE, TFT_BLACK, true);
-    if (distance > 0) {
-      displayDistance(distance);
-      // Serial.print("Distance: ");
-      // Serial.println(distance);
+    if (sensorAvailable()) {
+      // REAL SENSOR
+      displayDistance(realDistance, 0);
+      displayDistanceText(realDistance);
     }
-    r = r + 30;
+    // SIMULATION, always run
+    float smallest = MAX_DISTANCE;
+    float average = 0;
+    for (int i = 0; i < NUM_SIM_TARGETS; i++) {
+      simulateTarget(i);
+      displayDistance(simDistance[i], simAngle[i]);
+      if (simDistance[i] < smallest) smallest = simDistance[i];
+      average = (simDistance[i] + average) / 2;
+    }
+    displayDataText(NUM_SIM_TARGETS, average);
+    displayDistanceText(smallest);
+    r += 30;
     delay(25);
   }
 }
 
 void setup() {
   Serial.begin(115200);
+  // Simulation setup
+  for (int i = 0; i < NUM_SIM_TARGETS; i++) {
+    simDistance[i] = random(500, 4000) / 10.0;
+    simAngle[i] = random(-90, 90) * DEG_TO_RAD;
+    simSpeed[i] = random(1, 5);
+  }
   // Backlight setup
   pinMode(TFT_BL, OUTPUT);
-  digitalWrite(TFT_BL, HIGH);   // Turn backlight ON
+  digitalWrite(TFT_BL, HIGH); // Turn backlight ON
 
   // Display init
   tft.init();
-  tft.setRotation(1);           // Landscape
+  tft.setRotation(1); // Landscape
 
   // Esp_Now init
   WiFi.mode(WIFI_STA);
